0cc354e1e1c28521b65a0318ab76a9aea6b6526f
zy162 <zy162@duke.edu>
2024-02-20 22:53:33 -0500

    modify product part db

--- db/create.sql
+++ db/create.sql
@@ -4,13 +4,13 @@
 
 CREATE TABLE Product (
     p_productkey BIGINT PRIMARY KEY NOT NULL,
-    p_shortname VARCHAR(255) NOT NULL,
+    p_productname VARCHAR(255) NOT NULL,
     p_description TEXT NOT NULL,
     p_imageurl VARCHAR(255) NOT NULL,
     p_price DOUBLE PRECISION NOT NULL,
-    p_category_key BIGINT NOT NULL,
+    p_catkey BIGINT NOT NULL,
     p_link TEXT NOT NULL,
-    FOREIGN KEY (p_category_key) REFERENCES Category(cat_key)
+    FOREIGN KEY (p_catkey) REFERENCES Category(cat_catkey)
 );
 
 CREATE TABLE ProductSeller (
@@ -22,10 +22,11 @@ CREATE TABLE ProductSeller (
     ps_createtime DATE NOT NULL,
     PRIMARY KEY (ps_productkey, ps_sellerkey),
     FOREIGN KEY (ps_productkey) REFERENCES Product(p_productkey)
+    FOREIGN KEY (ps_sellerkey) REFERENCES Seller(s_sellerkey)
 );
 
 CREATE TABLE Category (
-    cat_key BIGINT PRIMARY KEY NOT NULL,
-    cat_name VARCHAR(255) NOT NULL
+    cat_catkey BIGINT PRIMARY KEY NOT NULL,
+    cat_catname VARCHAR(255) NOT NULL
 );
 
