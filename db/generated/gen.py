a3e1500766d8f6160439ae4616b9b2e9d034c1fb
wl256 <wl256@duke.edu>
2024-03-19 20:11:39 -0400

    complete gen py

--- db/generated/gen.py
+++ db/generated/gen.py
@@ -33,7 +33,7 @@ def get_csv_writer(f):
 #     return
 
 def gen_users(num_users):
-    with open('Users.csv', 'w') as f:
+    with open('./db/generated/Users.csv', 'w') as f:
         writer = get_csv_writer(f)
         print('Users...', end=' ', flush=True)
         for uid in range(num_users):
@@ -61,24 +61,31 @@ def gen_users(num_users):
 
 def gen_products(num_products):
     available_pids = []
-    with open('Products.csv', 'w') as f:
+    with open('./db/generated/Products.csv', 'w') as f:
         writer = get_csv_writer(f)
         print('Products...', end=' ', flush=True)
         for pid in range(num_products):
             if pid % 100 == 0:
                 print(f'{pid}', end=' ', flush=True)
             name = fake.sentence(nb_words=4)[:-1]
+            description = fake.text(max_nb_chars=200)
+            imageurl = fake.image_url()
             price = f'{str(fake.random_int(max=500))}.{fake.random_int(max=99):02}'
+            # price = round(fake.pydecimal(left_digits=3, right_digits=2, positive=True), 2)
+            catkey = fake.random_int(min=1, max=5)  # Assuming 5 categories exist
+            link = fake.url()
+
             available = fake.random_element(elements=('true', 'false'))
             if available == 'true':
                 available_pids.append(pid)
-            writer.writerow([pid, name, price, available])
+            # writer.writerow([pid, name, price, available])
+            writer.writerow([name, description, imageurl, price, catkey, link])
         print(f'{num_products} generated; {len(available_pids)} available')
     return available_pids
 
 
 def gen_purchases(num_purchases, available_pids):
-    with open('Purchases.csv', 'w') as f:
+    with open('./db/generated/Purchases.csv', 'w') as f:
         writer = get_csv_writer(f)
         print('Purchases...', end=' ', flush=True)
         for id in range(num_purchases):
