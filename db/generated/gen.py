0fd888202f5173fb66a40b88024b3043059fa3f7
zy161 <zy161@duke.edu>
2024-03-20 14:10:10 -0400

    generated new csv files

--- db/generated/gen.py
+++ db/generated/gen.py
@@ -5,6 +5,8 @@ from faker import Faker
 num_users = 100
 num_products = 2000
 num_purchases = 2500
+num_product_reviews = 500
+num_seller_reviews = 500
 
 Faker.seed(0)
 fake = Faker()
@@ -51,7 +53,7 @@ def gen_products(num_products):
     return available_pids
 
 
-def gen_purchases(num_purchases, available_pids):
+"""def gen_purchases(num_purchases, available_pids):
     with open('Purchases.csv', 'w') as f:
         writer = get_csv_writer(f)
         print('Purchases...', end=' ', flush=True)
@@ -63,9 +65,44 @@ def gen_purchases(num_purchases, available_pids):
             time_purchased = fake.date_time()
             writer.writerow([id, uid, pid, time_purchased])
         print(f'{num_purchases} generated')
+    return"""
+
+def gen_product_reviews(num_product_reviews):
+    with open('Product_Reviews.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Product_Reviews...', end=' ', flush=True)
+        for pr_productkey in range(num_products):
+            if pr_productkey % 10 == 0:
+                print(f'{pr_productkey}', end=' ', flush=True)
+            pr_userkey = f'{fake.random_int(max=500)}'
+            pr_productname = fake.sentence(nb_words=4)[:-1]
+            pr_orderkey = f'{str(fake.random_int(max=5000))}'
+            pr_reviewdate = fake.date_time()
+            pr_review = fake.sentence(nb_words=15)[:-1]
+            pr_rating = f'{str(fake.random_int(max=4))}.{fake.random_int(max=9):01}'
+            writer.writerow([pr_productkey, pr_userkey, pr_productname, pr_orderkey, pr_reviewdate, pr_review, pr_rating])
+        print(f'{num_product_reviews} generated')
     return
 
+def gen_seller_reviews(num_seller_reviews):
+    with open('Seller_Reviews.csv', 'w') as f:
+        writer = get_csv_writer(f)
+        print('Seller_Reviews...', end=' ', flush=True)
+        for sid in range(num_users):
+            if sid % 10 == 0:
+                print(f'{sid}', end=' ', flush=True)
+            uid = f'{fake.random_int(max=500)}'
+            seller_name = fake.sentence(nb_words=4)[:-1]
+            order_key = f'{str(fake.random_int(max=5000))}'
+            time_reviewed = fake.date_time()
+            review = fake.sentence(nb_words=15)[:-1]
+            rating = f'{str(fake.random_int(max=4))}.{fake.random_int(max=9):01}'
+            writer.writerow([sid, uid, seller_name, order_key, time_reviewed, review, rating])
+        print(f'{num_product_reviews} generated')
+    return
 
 gen_users(num_users)
 available_pids = gen_products(num_products)
-gen_purchases(num_purchases, available_pids)
+"""gen_purchases(num_purchases, available_pids)"""
+gen_product_reviews(num_product_reviews)
+gen_seller_reviews(num_seller_reviews)
--- db/load.sql
+++ db/load.sql
@@ -1,17 +1,26 @@
-/* \COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
--- since id is auto-generated; we need the next command to adjust the counter
--- for auto-generation so next INSERT will not clash with ids loaded above:
-SELECT pg_catalog.setval('public.users_id_seq',
-                         (SELECT MAX(id)+1 FROM Users),
+-- \COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
+-- -- since id is auto-generated; we need the next command to adjust the counter
+-- -- for auto-generation so next INSERT will not clash with ids loaded above:
+-- SELECT pg_catalog.setval('public.users_u_userkey_seq',
+--                          (SELECT MAX(u_userkey)+1 FROM Users),
+--                          false);
+
+\COPY Product FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.product_p_productkey_seq',
+                         (SELECT MAX(p_productkey)+1 FROM Product),
                          false);
 
-\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.products_id_seq',
-                         (SELECT MAX(id)+1 FROM Products),
-                         false);
+-- \COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.purchases_id_seq',
+--                          (SELECT MAX(id)+1 FROM Purchases),
+--                          false);
 
-\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.purchases_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
-                         false);
- */
+-- \COPY ProductReview FROM 'Product_Reviews.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.productreview_pr_productkey_seq',
+--                          (SELECT MAX(pr_productkey)+1 FROM ProductReview),
+--                          false);
+
+-- \COPY SellerReview FROM 'Seller_Reviews.csv' WITH DELIMITER ',' NULL '' CSV
+-- SELECT pg_catalog.setval('public.sellerreview_sr_sellerkey_seq',
+--                          (SELECT MAX(sr_sellerkey)+1 FROM SellerReview),
+--                          false);
