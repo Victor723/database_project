6d308aa11dcf0489ac0d146478f1e5d29d8c756b
wl256 <wl256@duke.edu>
2024-03-21 17:23:12 -0400

    t

---
+++ app/order_history.py
@@ -0,0 +1,9 @@
+# from flask import render_template
+# from flask_login import current_user
+# import datetime
+# from flask import jsonify
+
+# from flask import Blueprint
+# bp = Blueprint('index', __name__)
+
+# @bp.route('/order_history')
