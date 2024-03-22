d3e0be9f9acbb2c12c4d9a5bd756af074c2585bc
wl256 <wl256@duke.edu>
2024-03-21 20:04:27 -0400

    can input id

--- app/order_history.py
+++ app/order_history.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from flask_login import current_user
 import datetime
 from flask import jsonify
@@ -7,9 +7,15 @@ from .models.user import User
 from flask import Blueprint
 bp = Blueprint('order_history', __name__)
 
-@bp.route('/order_history')
+@bp.route('/order_history', methods=['GET', 'POST'])
 def order_history():
-    userkey = 3
-    order_history = User.get_order_history_by_id(userkey)
+    userkey = None
+    order_history_data = []
+
+    if request.method == 'POST':
+        userkey = request.form.get('userkey')
+        if userkey:
+            order_history_data = User.get_order_history_by_id(userkey)
+
     return render_template('order_history.html',
-                    order_history=order_history)
+                    order_history=order_history_data)
