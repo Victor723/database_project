37e7c482dea600735af3754b411aa845f3e1605c
zy162 <zy162@duke.edu>
2024-04-15 22:44:18 -0400

    comment out unfinished part first(review)

--- app/product_details.py
+++ app/product_details.py
@@ -14,11 +14,9 @@ bp = Blueprint('product_details', __name__)
 def product_details(product_id):
     product_details = Product.get_prod_details(product_id)
     # add review session
+    # had_review = ProductReview.get()
+    # bought = Order.get()
+    # can_add_review = not had_review & bought
 
-    had_review = ProductReview.get()
-    bought = Order.get()
-    can_add_review = not had_review & bought
     return render_template('product.html',
-                        product_details=product_details,
-                        
-                        can_add_review=can_add_review)
+                        product_details=product_details)
